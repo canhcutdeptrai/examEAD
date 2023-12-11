bb80bba4cdb6d4425c12301f94b7a669c902d0ae
canhcutdeptrai <93236370+canhcutdeptrai@users.noreply.github.com>
2023-12-11 23:30:02 +0700

    Delete src directory

--- src/main/java/com/example/multidb/customer/projection/CustomerProjection.java
+++
@@ -1,6 +0,0 @@
-package com.example.multidb.customer.projection;
-
-public interface CustomerProjection {
-    String getName();
-    String getPhoneNumber();
-}
--- src/main/java/com/example/multidb/customer/repository/PhoneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.multidb.customer.repository;
-
-import com.example.multidb.customer.entity.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhoneRepository extends JpaRepository<Phone,Long> {
-}
